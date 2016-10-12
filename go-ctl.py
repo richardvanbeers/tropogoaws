92da7cbf3f080e3c1d3da1705ed45c2b39d02271
Martin Kåberg <martin.kaberg@nordcloud.se>
2016-10-12 16:57:39 +0200

    Logger

--- go-ctl.py
+++ go-ctl.py
@@ -13,6 +13,7 @@ def setup_logger():
     logger.setLevel(logging.INFO)
     logging.getLogger('requests').setLevel(logging.WARNING)
     logging.getLogger('botocore').setLevel(logging.WARNING)
+    logging.getLogger('urllib').setLevel(logging.WARNING)
     return logger
 
 def make_tarfile(output_filename, source_dir):
