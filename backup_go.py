c92ca1ffdb5eac6da5e240973dfc84b261367e31
Richard van Beers <richard.vanbeers@nordcloud.com>
2016-10-12 10:37:06 +0200

    make tarfile

--- backup_go.py
+++ backup_go.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
-import requests
-import json
+import requests, json, tarfile, os
+
 from requests.auth import HTTPBasicAuth
 
 url = 'http://ec2-54-218-128-110.us-west-2.compute.amazonaws.com:8153/go/api/backups'
-
 headers = {'Confirm': 'true', 'Accept': 'application/vnd.go.cd.v1+json'}
+s3_bucket = 'my_s3_bucket'
+
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
 r = requests.post(url, auth=('admin', 'admin'), headers=headers)
@@ -18,11 +23,13 @@ if not r.status_code == 200:
     exit(2)
 
 response_object = json.loads(r.text)
-
 backup_path = response_object['path']  # use this form for immediate fail
+backup_dir = os.path.basename(backup_path)
+backup_file = backup_dir+".tgz"
 
-print backup_path
+make_tarfile(backup_file, backup_path)
 
+# print backup_path
 # print r.text
 # goal:
 # curl 'http://ec2-54-218-128-110.us-west-2.compute.amazonaws.com:8153/go/api/backups' -u 'admin:admin' -H 'Confirm: true' -H 'Accept: application/vnd.go.cd.v1+json' -X POST
