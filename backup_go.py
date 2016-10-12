c79bea915692f21c2c41632b92a89bb14ddaa6f2
Richard van Beers <richard.vanbeers@nordcloud.com>
2016-10-12 09:52:57 +0200

    cleanup

--- backup_go.py
+++ backup_go.py
@@ -3,14 +3,10 @@
 import requests
 import json
 from requests.auth import HTTPBasicAuth
+
 url = 'http://ec2-54-218-128-110.us-west-2.compute.amazonaws.com:8153/go/api/backups'
 
-# payload = json.load(open("request.json"))
 headers = {'Confirm': 'true', 'Accept': 'application/vnd.go.cd.v1+json'}
-# r = requests.post(url, data=json.dumps(payload), headers=headers)
-
-# curl 'http://ec2-54-218-128-110.us-west-2.compute.amazonaws.com:8153/go/api/backups' -u 'admin:admin' -H 'Confirm: true' -H 'Accept: application/vnd.go.cd.v1+json' -X POST
-
 
 
 r = requests.post(url, auth=('admin', 'admin'), headers=headers)
@@ -23,10 +19,10 @@ if not r.status_code == 200:
 
 response_object = json.loads(r.text)
 
+backup_path = response_object['path']  # use this form for immediate fail
 
-backup_path =  response_object['path'] # use this form for immidiate fail
-# print response_object.get('path')
 print backup_path
-print r.status_code
-print r.text
-# print json.dumps(r.json)
+
+# print r.text
+# goal:
+# curl 'http://ec2-54-218-128-110.us-west-2.compute.amazonaws.com:8153/go/api/backups' -u 'admin:admin' -H 'Confirm: true' -H 'Accept: application/vnd.go.cd.v1+json' -X POST
