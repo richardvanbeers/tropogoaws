5300fc4aa421114fa2c1999e1df31d0f392bb800
Martin Kåberg <martin.kaberg@nordcloud.se>
2016-10-12 17:07:37 +0200

    pep8 and removed ld debug prints

--- go-ctl.py
+++ go-ctl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import subprocess, requests, json, tarfile, os, boto3, argparse , logging
+import subprocess, requests, json, tarfile, os, boto3, argparse, logging
 import xml.etree.ElementTree as ET
 
 
@@ -16,6 +16,7 @@ def setup_logger():
     logging.getLogger('urllib3').setLevel(logging.WARNING)
     return logger
 
+
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
@@ -28,11 +29,11 @@ def backup(config, s3_bucket_name=None, prefix=None):
 
     if s3_bucket_name is None:
         s3_bucket_name = config['bucket']
-    else :
+    else:
         logger.info("Changed bucket to %s", s3_bucket_name)
     if prefix is None:
         prefix = config["prefix"]
-    else :
+    else:
         logger.info("Changed prefix to %s", prefix)
     headers = {'Confirm': 'true', 'Accept': 'application/vnd.go.cd.v1+json'}
 
@@ -48,7 +49,7 @@ def backup(config, s3_bucket_name=None, prefix=None):
     backup_file = "{}/{}.tgz".format(config["backup_path"], os.path.basename(backup_path))
     if not os.path.exists(os.path.dirname(backup_file)):
         os.makedirs(os.path.dirname(backup_file))
-        logger.info("Creating backup dir %s",os.path.dirname(backup_file))
+        logger.info("Creating backup dir %s", os.path.dirname(backup_file))
     make_tarfile(backup_file, backup_path)
     logger.info("Created tarball from %s in %s", backup_path, backup_file)
     os.chmod(backup_file, 0600)
@@ -73,7 +74,8 @@ def backup(config, s3_bucket_name=None, prefix=None):
     command = ['aws', 's3', 'sync', '{}/pipelines'.format(artifacts_dir),
                's3://{}/go_server/backups/pipelines'.format(s3_bucket_name)]
     subprocess.call(command, shell=False)
-    logger.info("Done syncing %s to %s",'{}/pipelines'.format(artifacts_dir), 's3://{}/go_server/backups/pipelines'.format(s3_bucket_name))
+    logger.info("Done syncing %s to %s", '{}/pipelines'.format(artifacts_dir),
+                's3://{}/go_server/backups/pipelines'.format(s3_bucket_name))
     command = ['service', 'go-server', 'start']
     subprocess.call(command, shell=False)
 
@@ -100,7 +102,6 @@ backup_parser.set_defaults(which="backup")
 
 args = parser.parse_args()
 
-print args
 config = {
     "backup_path": "/tmp/go-backups",
     "prefix": "go_server/backups",
@@ -112,13 +113,11 @@ if os.path.isfile(args.config_file):
         config = json.load(data_file)
 
 if args.which == "set":
-    print "We now set {} = {}".format(args.key, args.value)
     config[args.key] = args.value
     with open(args.config_file, 'w') as outfile:
         json.dump(config, outfile)
     os.chmod(args.config_file, 0600)
 elif args.which == "get":
-    print "Now we get the value of {}".format(args.key)
     print config.get(args.key)
 elif args.which == "backup":
     backup(config, s3_bucket_name=args.bucket, prefix=args.prefix)
